af31326f16880d24eda183827dc42db05ed57c31
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-06 06:13:21 +0900

    [Feat] ViewPresenter.cs 생성
    UIBar과 연결해줄 Presenter 클래스 생성

---
+++ Assets/02. Scripts/ViewPresenter.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using VInspector.Libs;
+
+public class ViewPresenter
+{
+    private IViewHpBar viewHpBar;
+    private IHpModel HpModel;
+
+    public ViewPresenter(IViewHpBar viewHpBar, IHpModel hpModel)
+    {
+        this.viewHpBar = viewHpBar;
+        this.HpModel = hpModel;
+    }
+
+    public void Init(Transform trParent)
+    {
+        viewHpBar.Init(trParent);
+    }
+
+    public void UpdateHealthBar()
+    {
+        viewHpBar.ShowHealth((float)HpModel.ModelCurHp / HpModel.ModelMaxHp);
+    }
+}
+
+public interface IHpModel
+{
+    public int ModelMaxHp { get;}
+    public int ModelCurHp { get;}
+}
