60f277aa2e48c4894e726396c5edc7048e7eeac0
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-06 06:00:20 +0900

    [Feat] Item.cs 생성
    아이템의 데이터를 가지고 있을 클래스 생성.

---
+++ Assets/02. Scripts/Item/Item.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string ItemID;
+    private ItemData itemData;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (1 << collision.gameObject.layer == ReadonlyData.PlayerLayerMask)
+        {
+            ManagerHub.Instance.PlayerManager.Player.PlayerUseItem.UseItem(itemData);
+            ReturnPool();
+        }
+    }
+
+
+    /// <summary>
+    /// 풀로 데이터를 반환하는 메서드.
+    /// </summary>
+    private void ReturnPool()
+    {
+        gameObject.SetActive(false);
+        ManagerHub.Instance.PoolManager.ReturnPool<Item>(ItemID, this);
+    }
+
+
+    public void Init(Vector2 startPosition)
+    {
+        transform.position = startPosition;
+        gameObject.SetActive(true);
+    }
+}
