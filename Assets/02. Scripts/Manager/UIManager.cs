b8cbb56f559fecc799b9e5ce4fb50775fda8261d
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-06 19:55:29 +0900

    [Feat] UI 오픈 버튼, 도감 추가

--- Assets/02. Scripts/Manager/UIManager.cs
+++ Assets/02. Scripts/Manager/UIManager.cs
@@ -3,9 +3,25 @@ using UnityEngine;
 public class UIManager
 {
     public Canvas Canvas { get; private set; }
+    public UIEnemyGuide UIEnemyGuide { get; private set; }
+    public UIOptionOpen OptionOpen { get; private set; }
+    public bool isOpenUI = false;
+
+    public void InitCanvas()
+    {
+        Canvas = Object.Instantiate(ManagerHub.Instance.AddressableManager.CanvasPrefab);
+    }
 
     public void Init()
     {
-        Canvas = Object.Instantiate(ManagerHub.Instance.AddressableManager.CanvasPrefab);
+        UIEnemyGuide = Object.Instantiate(ManagerHub.Instance.AddressableManager.UIEnemyGuidePrefab, Canvas.transform);
+        OptionOpen = Object.Instantiate(ManagerHub.Instance.AddressableManager.UIOptionOpenPrefab, Canvas.transform);
+    }
+
+
+    public void SetActiveUI(bool isActive)
+    {
+        PoolHub hpBarHub = ManagerHub.Instance.PoolManager.dictTypeToPool[typeof(HpBarView)];
+        hpBarHub?.SetActiveParentObj(isActive);
     }
 }
