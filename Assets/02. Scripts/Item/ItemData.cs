e0734a9e778b0a53d2ef3e95103e8c9d3b6029d2
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-06 19:52:10 +0900

    [Feat] 복사 가능한 생성자 추가

--- Assets/02. Scripts/Item/ItemData.cs
+++ Assets/02. Scripts/Item/ItemData.cs
@@ -1,17 +1,35 @@
 [System.Serializable]
 public class ItemData
 {
-    public string ItemID;
-    public string Name;
-    public string Description;
-    public int UnlockLev;
-    public int MaxHP;
-    public float MaxHPMul;
-    public int MaxMP;
-    public float MaxMPMul;
-    public int MaxAtk;
-    public float MaxAtkMul;
-    public int MaxDef;
-    public float MaxDefMul;
-    public int Status;
+    public string   ItemID;
+    public string   Name;
+    public string   Description;
+    public int      UnlockLev;
+    public int      MaxHP;
+    public float    MaxHPMul;
+    public int      MaxMP;
+    public float    MaxMPMul;
+    public int      MaxAtk;
+    public float    MaxAtkMul;
+    public int      MaxDef;
+    public float    MaxDefMul;
+    public int      Status;
+
+    public ItemData() { }
+    public ItemData(ItemData itemData)
+    {
+        this.ItemID = itemData.ItemID;
+        this.Name = itemData.Name;
+        this.Description = itemData.Description;
+        this.UnlockLev = itemData.UnlockLev;
+        this.MaxHP = itemData.MaxHP;
+        this.MaxHPMul = itemData.MaxHPMul;
+        this.MaxMP = itemData.MaxMP;
+        this.MaxMPMul = itemData.MaxMPMul;
+        this.MaxAtk = itemData.MaxAtk;
+        this.MaxAtkMul = itemData.MaxAtkMul;
+        this.MaxDef = itemData.MaxDef;
+        this.MaxDefMul  = itemData.MaxDefMul;
+        this.Status = itemData.Status;
+    }
 }
