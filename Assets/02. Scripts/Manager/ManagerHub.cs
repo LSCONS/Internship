4974a834fd65ae5e7283ec463b5a6008922881c0
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-06 19:54:42 +0900

    [Refactor] 캔버스 초기화 기능 분리

--- Assets/02. Scripts/Manager/ManagerHub.cs
+++ Assets/02. Scripts/Manager/ManagerHub.cs
@@ -2,20 +2,22 @@ using UnityEngine;
 
 public class ManagerHub : Singleton<ManagerHub>
 {
+    [field: SerializeField] public EnemySpawnerManager EnemySpawnerManager { get; private set; }
     public AddressableManager AddressableManager { get; private set; } = new();
     public PoolManager PoolManager { get; private set; } = new();
     public DataManager DataManager { get; private set; } = new();
     public PlayerManager PlayerManager { get; private set; } = new();
-    public EnemySpawnerManager EnemySpawnerManager { get; private set; } = new();
     public UIManager UIManager { get; private set; } = new();
 
     protected override async void Awake()
     {
         base.Awake();
         await AddressableManager.Init();
-        UIManager.Init();
+        UIManager.InitCanvas();
         DataManager.Init();
         PoolManager.Init();
+
+        UIManager.Init();
         PlayerManager.Init();
         EnemySpawnerManager.Init();
     }
