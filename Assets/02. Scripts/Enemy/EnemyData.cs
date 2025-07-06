5cd37d5615572c4d80e7e0abf6f93e85b7a62de2
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-06 19:52:00 +0900

    [Feat] 복사 가능한 생성자 추가

--- Assets/02. Scripts/Enemy/EnemyData.cs
+++ Assets/02. Scripts/Enemy/EnemyData.cs
@@ -1,28 +1,46 @@
 [System.Serializable]
 public class EnemyData : IHpModel
 {
-    public string MonsterID;
-    public string Name;
-    public string Description;
-    public int Attack;
-    public float AttackMul;
-    public int MaxHP;
-    public float MaxHPMul;
-    public int AttackRange;
-    public float AttackRangeMul;
-    public float AttackSpeed;
-    public float MoveSpeed;
-    public int MinExp;
-    public int MaxExp;
-    public int[] DropItem;
+    public string   MonsterID;
+    public string   Name;
+    public string   Description;
+    public int      Attack;
+    public float    AttackMul;
+    public int      MaxHP;
+    public float    MaxHPMul;
+    public int      AttackRange;
+    public float    AttackRangeMul;
+    public float    AttackSpeed;
+    public float    MoveSpeed;
+    public int      MinExp;
+    public int      MaxExp;
+    public int[]    DropItem;
 
     public int CurHp;
+    public int ModelMaxHp => MaxHP;
+    public int ModelCurHp => CurHp;
 
     public void Init()
     {
         CurHp = MaxHP;
     }
 
-    public int ModelMaxHp => MaxHP;
-    public int ModelCurHp => CurHp;
+    public EnemyData() { }
+    public EnemyData(EnemyData other)
+    {
+        this.MonsterID = other.MonsterID;
+        this.Name = other.Name;
+        this.Description = other.Description;
+        this.Attack = other.Attack;
+        this.AttackMul = other.AttackMul;
+        this.MaxHP = other.MaxHP;
+        this.MaxHPMul = other.MaxHPMul;
+        this.AttackRange = other.AttackRange;
+        this.AttackRangeMul = other.AttackRangeMul;
+        this.AttackSpeed = other.AttackSpeed;
+        this.MoveSpeed = other.MoveSpeed;
+        this.MinExp = other.MinExp;
+        this.MaxExp = other.MaxExp;
+        this.DropItem = other.DropItem;
+    }
 }
