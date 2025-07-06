d1181333612127c9550d9753f4344eb78f17f9e7
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-06 20:52:09 +0900

    [Feat] 몬스터 멈추는 기능 추가

--- Assets/02. Scripts/Enemy/EnemyMove.cs
+++ Assets/02. Scripts/Enemy/EnemyMove.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class EnemyMove
 {
     private Enemy enemy;
+    private bool canMove { get; set; } = true;
+
+
     public EnemyMove(Enemy enemy)
     {
         this.enemy = enemy;
@@ -11,15 +14,29 @@ public class EnemyMove
 
     public void FixedUpdate()
     {
-        Move();
+        if (canMove) Move();
+        else enemy.Rigid2D.linearVelocity = Vector3.zero;
     }
 
 
+    /// <summary>
+    /// 몬스터가 플레이어를 향해 움직이게 하는 메서드
+    /// </summary>
     private void Move()
     {
         Vector2 playerPoint = ManagerHub.Instance.PlayerManager.Player.transform.position;
         Vector2 enemyPoint = enemy.transform.position;
         Vector2 moveDir = (playerPoint - enemyPoint).normalized;
-        enemy.Rigid2D.linearVelocity = enemy.enemyData.MoveSpeed * moveDir;
+        enemy.Rigid2D.linearVelocity = enemy.EnemyData.MoveSpeed * moveDir;
+    }
+
+
+    /// <summary>
+    /// 몬스터의 움직임을 결정하는 메서드
+    /// </summary>
+    /// <param name="isMove">몬스터 움직임 여부</param>
+    public void SetMove(bool isMove)
+    {
+        canMove = isMove;
     }
 }
