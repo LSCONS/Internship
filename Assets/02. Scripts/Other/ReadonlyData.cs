ab8176eab446d4c74c8cccf91b27b862f84832eb
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-07 04:37:24 +0900

    [Feat] Animator Hash 변수 추가.

--- Assets/02. Scripts/Other/ReadonlyData.cs
+++ Assets/02. Scripts/Other/ReadonlyData.cs
@@ -4,4 +4,8 @@ public static class ReadonlyData
 {
     public readonly static LayerMask EnemyLayerMask = 1 << LayerMask.NameToLayer("Enemy");
     public readonly static LayerMask PlayerLayerMask = 1 << LayerMask.NameToLayer("Player");
+
+    public readonly static int AnimatorHash_IsDie = Animator.StringToHash("IsDie");
+    public readonly static int AnimatorHash_IsMove = Animator.StringToHash("IsMove");
+    public readonly static int AnimatorHash_IsIdle = Animator.StringToHash("IsIdle");
 }
