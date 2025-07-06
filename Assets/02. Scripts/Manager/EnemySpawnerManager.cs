558a4bf2df51319e544437f526fe3bd6755189ee
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-06 19:54:06 +0900

    [Feat] 스폰 반복 기능 추가

--- Assets/02. Scripts/Manager/EnemySpawnerManager.cs
+++ Assets/02. Scripts/Manager/EnemySpawnerManager.cs
@@ -1,24 +1,38 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
+[Serializable]
 public class EnemySpawnerManager
 {
+    private WaitForSeconds waitEnemySpawn;
+    public float SpawnCooldown = 10;        //몬스터 스폰 쿨타임
+    public float SpawnDistance = 5;         //플레이어로부터 몬스터 스폰 거리
+    private Coroutine enemySpawnCoroutine;
+
     public void Init()
     {
-        SpawnEnemy("M0010", new Vector2(-5, 0));
+        waitEnemySpawn = new WaitForSeconds(SpawnCooldown);
+        enemySpawnCoroutine = ManagerHub.Instance.StartCoroutine(RepeatSpawnCoroutine());
     }
 
 
-    private void SpawnEnemy(string monsterID, Vector2 position)
+    /// <summary>
+    /// 특정 시간마다 랜덤으로 몬스터를 특정 위치에 스폰하는 메서드
+    /// </summary>
+    private IEnumerator RepeatSpawnCoroutine()
     {
-        Enemy enemy = ManagerHub.Instance.PoolManager.GetPoolObject<Enemy>(monsterID);
-
-        if(enemy == null)
+        while (true)
         {
-            Debug.LogError($"Enemy 클래스를 찾을 수 없습니다. ID: {monsterID}");
-            return;
+            Enemy spawnEnemy = ManagerHub.Instance.PoolManager.GetRandomEnemy();
+            if(spawnEnemy != null)
+            {
+                Transform trPlayer = ManagerHub.Instance.PlayerManager.Player.transform;
+                Vector2 spawnPosition = trPlayer.position.RandomPointOnCircle(SpawnDistance);
+                spawnEnemy.Init();
+                spawnEnemy.SetPosition(spawnPosition);
+            }
+            yield return waitEnemySpawn;
         }
-
-        enemy.Init();
-        enemy.SetPosition(position);
     }
 }
