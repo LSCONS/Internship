02aec3ecc146a3090f0597a5f0059222c795d4e2
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-06 19:52:19 +0900

    [Chore] 주석 추가

--- Assets/02. Scripts/HpBarView.cs
+++ Assets/02. Scripts/HpBarView.cs
@@ -24,12 +24,19 @@ public class HpBarView : MonoBehaviour, IViewHpBar
     }
 
 
+    /// <summary>
+    /// HpBar의 상태를 입력 값에 따라 초기화하는 메서드.
+    /// </summary>
+    /// <param name="amount">입력할 수치</param>
     public void ShowHealth(float amount)
     {
         ImgHpBar.fillAmount = amount;
     }
 
 
+    /// <summary>
+    /// HpBar오브젝트를 풀로 되돌리는 메서드.
+    /// </summary>
     public void ReturnPool()
     {
         gameObject.SetActive(false);
